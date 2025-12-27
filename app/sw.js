e3e2331ca8c355c1ba9fd2075eae7520f4b0a379
domolink2796-gif <domolink_2796@mail.ru>
2025-12-28 00:54:17 +0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,3 +1,16 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
+{
+  "name": "Logist_X",
+  "short_name": "Logist_X",
+  "start_url": "./index.html",
+  "scope": "./",
+  "display": "standalone",
+  "background_color": "#010409",
+  "theme_color": "#010409",
+  "icons": [
+    {
+      "src": "icon-1024.png",
+      "sizes": "1024x1024",
+      "type": "image/png"
+    }
+  ]
+}
