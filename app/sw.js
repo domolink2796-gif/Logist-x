0148bfb5ec0692d95a0d10032139b03bfb83c9ce
domolink2796-gif <domolink_2796@mail.ru>
2025-12-28 02:08:38 +0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,3 +1,14 @@
+const CACHE_NAME = 'logist-x-v1';
+const assets = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+    );
+});
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
