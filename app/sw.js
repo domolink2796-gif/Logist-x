0005ae902115ef448d12c7b80ee2ae35f7ea2a65
domolink2796-gif <domolink_2796@mail.ru>
2025-12-28 03:54:38 +0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,56 +1,14 @@
-/* LOGIST_X SERVICE WORKER v4.50 */
-const CACHE_NAME = 'logistx-core-v450';
-const OFFLINE_URL = 'index.html';
+const CACHE_NAME = 'logistx-v1';
 
-// Список файлов для кэширования при установке
-const ASSETS_TO_CACHE = [
-  './index.html',
-  './manifest.json',
-  './icon-1024.png'
-];
-
-// Установка: создание кэша и принудительная активация
 self.addEventListener('install', (event) => {
-  console.log('[SW] Install Event');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[SW] Caching app shell');
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
-// Активация: очистка старых версий кэша
 self.addEventListener('activate', (event) => {
-  console.log('[SW] Activate Event');
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[SW] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
+    event.waitUntil(clients.claim());
 });
 
-// Перехват запросов: стратегия "Сначала сеть, потом кэш"
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(OFFLINE_URL);
-      })
-    );
-    return;
-  }
-  
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    // Просто пропускаем запросы, этого достаточно для установки PWA
+    event.respondWith(fetch(event.request));
 });
